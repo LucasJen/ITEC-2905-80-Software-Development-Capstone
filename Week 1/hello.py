501a2ddd2e3fb02432682dfad6c55886908ddde2
LucasJen <lucasjjennings@gmail.com>
2026-01-13 22:22:41 +0000

    week1

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -12,5 +12,14 @@ if monthOfBirth == "January":
 else:
     print("It is not your birth month.")
 
-
 #Part 2 -- Lists of classes
+
+class_list = [] # creates an empty list to store future data
+class_name = input('Input a class that you are taking this semester. Enter to quit. ')
+while class_name: # loop through to add elements to the list
+    class_list.append(class_name)
+    class_name = input('Input a class that you are taking this semester. Enter to quit. ')
+print(class_list) # print entire list
+
+for c in class_list: # loop through and print each class name, one per line.
+    print(c) #print list, one per line.
