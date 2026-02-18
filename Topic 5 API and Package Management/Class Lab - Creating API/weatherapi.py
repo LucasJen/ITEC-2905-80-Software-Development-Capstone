f65f49fe969e94e4ebbbbaccf6e4819c5d6c5714
LucasJen <fake@fake.com>
2026-02-18 15:22:23 -0600

    error handling comments

--- Topic 5 API and Package Management/Class Lab - Creating API/weatherapi.py
+++ Topic 5 API and Package Management/Class Lab - Creating API/weatherapi.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from pprint import pprint
+import datetime
 
 # Minneapolis
 lat = 44.97
@@ -14,3 +15,28 @@ url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&uni
 response = requests.get(url)
 weather_forecast = response.json()
 pprint(weather_forecast)
+
+list_forecast = weather_forecast['list']
+for forecast in list_forecast:
+    temp = forecast['main']['temp']
+    timestamp = forecast['dt']
+    weather_description = forecast['weather'][0]['description']
+    wind_speed = forecast['wind']['speed']
+    date = datetime.fromtimestamp(timestamp).strftime("%m-%d-%Y at %I:%M %p")
+    print(f'In Minneapolis, on {date}, the temperature will be {temp:.2f}F.\n'
+    f'The weather will be {weather_description}, and the wind speed will be {wind_speed:.2f} mph.\n')
+
+
+# error handling:
+
+"""
+
+when connecting to api (url)
+when querying api
+when searching api for specific terms
+potential error handling for env variables
+error handling for location data, are the coords real?
+any input validation for user input
+
+
+"""
