1ed927a34ee0177865173bdffd5be25fb726ad9c
LucasJen <fake@fake.com>
2026-02-18 13:09:28 -0600

    removed duplicate dir

--- Topic 5 API and Package Management/hello_api/env/Scripts/activate.bat
+++ Topic 5 API and Package Management/hello_api/env/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\MistrasFHR-PB2\Desktop\Coding\ITEC-2905-80-Software-Development-Capstone\Topic 5 API and Package Management\hello_api\env"
+set "VIRTUAL_ENV=C:\Users\lucas\Desktop\Coding\ITEC-2905-80-Software-Development-Capstone\Topic 5 Package Management, APIs\hello_api\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(env) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(env) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
-set "VIRTUAL_ENV_PROMPT=(env) "
+set "VIRTUAL_ENV_PROMPT=env"
 
 :END
 if defined _OLD_CODEPAGE (
