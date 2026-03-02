05d9db0cb024fde9ea5fe68e1954d6bd68427754
LucasJen <fake@fake.com>
2026-03-02 07:25:29 -0600

    tutor

--- Topic 7 Artificial Intelligence - Integrating AI into Python Apps/tutor.py
+++ Topic 7 Artificial Intelligence - Integrating AI into Python Apps/tutor.py
@@ -7,7 +7,7 @@ client = genai.Client()
 chat = client.chats.create(model='gemini-2.5-flash')
 
 while True:
-    question = input('Enter your message for the tutor')
+    question = input('Enter your message for the tutor ')
     response = chat.send_message(
         # model='gemini-2.5-flash',
         question,
